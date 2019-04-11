42a64be1d7799425aa8bb6d991c009fd0d919939
ilyaabramovich <ilyaabramovich1998@gmail.com>
2019-04-11 16:30:32 +0800

    params for GET; update POST solutions logic

--- src/master.js
+++ src/master.js
@@ -22,13 +22,18 @@ app.use(bodyParser.json());
 app.post('/solutions', (req, res) => {
   const id = createHash(req.body.source);
   const sourceFile = getSourcePath(id);
-  const metaFile = getMetaPath(id);
+  const destDir = getTasksDirPath(id);
+  const metaFile = path.join(destDir, '/meta.json');
   Promise.all([
     mkdir(path.dirname(sourceFile), { recursive: true }),
-    writeFile(sourceFile, req.body.source),
     mkdir(path.dirname(metaFile), { recursive: true }),
-    writeFile(metaFile, JSON.stringify({ id, lang: 'java' })),
   ])
+    .then(
+      Promise.all([
+        writeFile(sourceFile, req.body.source),
+        writeFile(metaFile, JSON.stringify({ task: 'compile', lang: 'java' })),
+      ]),
+    )
     .then(() => {
       res.send({ result: { id }, ...STATUS.ok });
     })
@@ -37,8 +42,8 @@ app.post('/solutions', (req, res) => {
     });
 });
 
-app.get('/solutions', (req, res) => {
-  const { id } = req.body;
+app.get('/solutions/:id', (req, res) => {
+  const { id } = req.params;
   const metaFile = getMetaPath(id);
   readFile(metaFile, { encoding: 'utf8' })
     .then((data) => {
@@ -77,8 +82,8 @@ app.post('/tests', (req, res) => {
     });
 });
 
-app.get('/tests', (req, res) => {
-  const { id } = req.body;
+app.get('/tests/:id', (req, res) => {
+  const { id } = req.params;
   readFile(getTestsDirPath(id), { encoding: 'utf8' })
     .then((data) => {
       res.send({ ...JSON.parse(data), ...STATUS.ok });
@@ -99,10 +104,10 @@ app.delete('/tests', (req, res) => {
     });
 });
 
-app.get('/run', (req, res) => {
-  const { id } = req.body;
-  const sourceFile = getSourcePath(id);
-  const destDir = getTasksDirPath(id);
+app.get('/run/:solution&:test', (req, res) => {
+  const { solution, test } = req.body;
+  const sourceFile = getSourcePath(solution);
+  const destDir = getTasksDirPath(solution);
   mkdir(destDir, { recursive: true });
   copyFile(sourceFile, path.join(destDir, '/Main.java'))
     .then(() => {
