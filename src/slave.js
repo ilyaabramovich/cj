968db90986e2b0a9884d334b60067437b3037cf2
ilyaabramovich <ilyaabramovich1998@gmail.com>
2019-04-14 13:57:42 +0800

    move runs dir logic  to master

--- src/slave.js
+++ src/slave.js
@@ -1,20 +1,17 @@
-const fs = require('fs').promises;
+const { readFile, writeFile } = require('fs').promises;
 const util = require('util');
 const rimraf = util.promisify(require('rimraf'));
 const chokidar = require('chokidar');
 const path = require('path');
 const { exec } = require('child_process');
 const {
-  getTasksDirPath,
-  getRunsDirPath,
-  getSolutionsDirPath,
-  STATUS,
+  getTasksDirPath, getRunsDirPath, getSolutionsDirPath, STATUS,
 } = require('./utils');
 
 async function updateMeta(dir, patch) {
   const metaFile = path.join(dir, 'meta.json');
-  const meta = JSON.parse(await fs.readFile(metaFile));
-  return fs.writeFile(metaFile, JSON.stringify({ ...meta, ...patch }));
+  const meta = JSON.parse(await readFile(metaFile));
+  return writeFile(metaFile, JSON.stringify({ ...meta, ...patch }));
 }
 
 // TODO: generalize taskCompile & taskRun
@@ -36,27 +33,22 @@ function taskCompile(dir, meta) {
         }
         await updateMeta(solutionDir, STATUS.ok);
         await rimraf(dir);
-        resolve();
-      }
+        return resolve();
+      },
     );
   });
 }
 
 function taskRun(dir, meta) {
-  const { id, lang, solution, test } = meta;
   const runsDir = getRunsDirPath(meta.id);
   console.log('running...');
   return new Promise(async (resolve, reject) => {
-    await fs.mkdir(runsDir, { recursive: true });
-    await fs.writeFile(
-      path.join(runsDir, 'meta.json'),
-      JSON.stringify({ id, lang, solution, test, ...STATUS.processing }),
-    );
+    await updateMeta(runsDir, STATUS.processing);
     const cp = exec(
       '"C:\\Program Files\\Java\\jdk-11.0.2\\bin\\java" Main',
       { cwd: dir },
       async (err, stdout, stderr) => {
-        const output = (await fs.readFile(path.join(dir, 'output.txt'))) + '';
+        const output = await readFile(path.join(dir, 'output.txt'), 'utf8');
         console.log(err, stdout, stderr);
         if (err) {
           await updateMeta(runsDir, STATUS.error);
@@ -66,29 +58,27 @@ function taskRun(dir, meta) {
         const checkResult = +(output.trim() === stdout.trim());
         await updateMeta(runsDir, { checkResult, ...STATUS.ok });
         await rimraf(dir);
-        resolve();
-      }
+        return resolve();
+      },
     );
-    const input = await fs.readFile(path.join(dir, 'input.txt'));
+    const input = await readFile(path.join(dir, 'input.txt'));
     cp.stdin.write(input);
     cp.stdin.end();
   });
 }
 
 function sleep(ms) {
-  return new Promise(resolve => {
+  return new Promise((resolve) => {
     setTimeout(() => resolve(), ms);
   });
 }
 
 async function main() {
   const watcher = chokidar.watch(getTasksDirPath(), { usePolling: true });
-  watcher.on('addDir', async fileName => {
+  watcher.on('addDir', async (fileName) => {
     console.log(fileName);
     await sleep(1000);
-    const meta = JSON.parse(
-      await fs.readFile(path.join(fileName, 'meta.json'))
-    );
+    const meta = JSON.parse(await readFile(path.join(fileName, 'meta.json')));
     console.log(meta);
     if (meta.task === 'compile') await taskCompile(fileName, meta);
     else if (meta.task === 'run') await taskRun(fileName, meta);
