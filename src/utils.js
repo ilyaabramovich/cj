ad8838193faec4a24f11da846890e192fbff8340
ilyaabramovich <ilyaabramovich1998@gmail.com>
2019-04-14 13:56:25 +0800

    remove useless functions

--- src/utils.js
+++ src/utils.js
@@ -1,58 +1,42 @@
-/* eslint-disable no-restricted-syntax */
 const path = require('path');
-const fs = require('fs');
+const { readdir, symlink } = require('fs').promises;
 const crypto = require('crypto');
 
-const ROOT_DIR = 'C:\\cj';
+const ROOT_DIR = path.join(__dirname, '..');
 const PORT = 3000;
 
 const STATUS = {
-  ok: { status: 'ok', statusCode: 0 }, // queued,
+  ok: { status: 'ok', statusCode: 0 },
   error: { status: 'error', statusCode: 1 },
   queue: { status: 'queue', statusCode: 2 },
   processing: { status: 'processing', statusCode: 3 },
 };
 
-// TODO: удалить неиспользуемые функции
-const getTasksDirPath = (id='.') => path.join(ROOT_DIR, 'tasks', id);
-const getSolutionsDirPath = id => path.join(ROOT_DIR, `./solutions/${id}/`);
+const getTasksDirPath = (id = '.') => path.join(ROOT_DIR, 'tasks', id);
+const getSolutionsDirPath = id => path.join(ROOT_DIR, 'solutions', id);
 const getRunsDirPath = id => path.join(ROOT_DIR, 'runs', id);
-const getTestsDirPath = id => path.join(ROOT_DIR, `./tests/${id}/`);
-const getSourcePath = id => path.join(getSolutionsDirPath(id), 'Main.java');
-const getMetaPath = id => path.join(getTasksDirPath(id), 'meta.json');
-const getTestInputPath = id => path.join(getTestsDirPath(id), 'input.txt');
-const getTestOutputPath = id => path.join(getTestsDirPath(id), 'output.txt');
+const getTestsDirPath = id => path.join(ROOT_DIR, 'tests', id);
 
-const createHash = data =>
-  crypto
-    .createHash('sha1')
-    .update(data)
-    .digest('hex');
-
-
+const createHash = data => crypto
+  .createHash('sha1')
+  .update(data)
+  .digest('hex');
 
 async function copyFiles({ src, dst, exclude = [] }) {
-  const files = await fs.promises.readdir(src);
+  const files = await readdir(src);
   const filesToCopy = files.filter(file => !exclude.includes(file));
-  // eslint-disable-next-line prefer-const
-  for (let file of filesToCopy) {
-    // eslint-disable-next-line no-await-in-loop
-    await fs.promises.symlink(path.join(src, file), path.join(dst, file));
-  }
+  await Promise.all(
+    filesToCopy.map(file => symlink(path.join(src, file), path.join(dst, file))),
+  );
 }
-// taskId
 
 module.exports = {
   createHash,
-  getSourcePath,
-  getMetaPath,
-  getTestInputPath,
-  getTestOutputPath,
   getSolutionsDirPath,
   getTestsDirPath,
   getTasksDirPath,
   getRunsDirPath,
+  copyFiles,
   STATUS,
   PORT,
-  copyFiles,
 };
